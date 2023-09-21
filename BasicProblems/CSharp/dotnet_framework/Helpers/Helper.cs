e6bc20dbb82fc0adcd030127cba2bdc723fa1741
rab.peti <43448373+TheH3ROo@users.noreply.github.com>
2023-09-21 18:32:09 +0200

    Add Helper class

---
+++ BasicProblems/CSharp/dotnet_framework/Helpers/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Helpers
+{
+    public static class Helper
+    {
+        public static int GetNumberFromUser()
+        {
+            int number = 0;
+            while (number < 1)
+            {
+                Console.Write("Please give me a number:");
+                int.TryParse(Console.ReadLine(), out number);
+            }
+            return number;
+        }
+    }
+}
