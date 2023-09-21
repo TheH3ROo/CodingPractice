e750bd99a88e47d55145fc63a4d4fccbfc7ffee8
rab.peti <43448373+TheH3ROo@users.noreply.github.com>
2023-09-21 18:34:57 +0200

    Finished FizzBuzz

--- BasicProblems/CSharp/dotnet_framework/FizzBuzz/Program.cs
+++ BasicProblems/CSharp/dotnet_framework/FizzBuzz/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Helpers;
+using System;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace FizzBuzz
 {
@@ -10,6 +8,42 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the FizzBuzz word game!\n");
+
+            var input_number = Helper.GetNumberFromUser();
+            FizzBuzz(input_number);
+            FizzBuzzStringBuilder(input_number);
+
+            Console.WriteLine("Thanks for playing!");
+            Console.Read();
+        }
+
+        static void FizzBuzz(int number)
+        {
+            Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0) Console.WriteLine("FizzBuzz");
+                else if (i % 3 == 0) Console.WriteLine("Fizz");
+                else if (i % 5 == 0) Console.WriteLine("Buzz");
+                else Console.WriteLine(i);
+            }
+            Console.WriteLine("\n");
+        }
+
+        static void FizzBuzzStringBuilder(int number)
+        {
+            Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i <= number; i++)
+            {
+                sb.Clear();
+                if (i % 3 == 0) sb.Append("Fizz");
+                if (i % 5 == 0) sb.Append("Buzz");
+                if(sb.Length == 0) sb.Append(i.ToString());
+                Console.WriteLine(sb.ToString());
+            }
+            Console.WriteLine("\n");
         }
     }
 }
