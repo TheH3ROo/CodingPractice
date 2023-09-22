ed5f0589ff6b34efaeffbfa75d27bc6a40cf39d3
rab.peti <43448373+TheH3ROo@users.noreply.github.com>
2023-09-22 14:35:17 +0200

    FizzBuzz finished two ways

--- BasicProblems/Rust/fizz_buzz/src/main.rs
+++ BasicProblems/Rust/fizz_buzz/src/main.rs
@@ -1,17 +1,40 @@
-use std::io::{stdout, BufWriter};
-
-use ferris_says::say;
+use std::io;
 
 fn main() {
     println!("Hello, world!");
-    ferris_says_to_console(String::from("Hello fellow Rustaceans!"));
+    fizz_buzz_to_n(30);
+    println!("");
+    fizz_buzz_concat_to_n(30);
+    io::stdin().read_line(&mut String::new()).unwrap();
 }
 
-fn ferris_says_to_console(message: String){
-    let stdout = stdout();
-    let message = String::from(message);
-    let width = message.chars().count();
+fn fizz_buzz_to_n(number: i32){
+    for n in 1..=number  {
+        if n % 3 == 0 && n % 5 == 0 {
+            println!("FizzBuzz")
+        } else if n % 3 == 0 {
+            println!("Fizz")
+        } else if n % 5 == 0 {
+            println!("Buzz")
+        } else{
+            println!("{}", n);
+        }
+    }
+}
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(&message, width, &mut writer).unwrap();
+fn fizz_buzz_concat_to_n(number: i32){
+    let mut result;
+    for n in 1..=number  {
+        result = String::new();
+        if n % 3 == 0 {
+            result.push_str("Fizz");
+        } 
+        if n % 5 == 0 {
+            result.push_str("Buzz");
+        } 
+        if result.is_empty(){
+            result.push_str(&n.to_string());
+        }
+        println!("{}", result);
+    }
 }
