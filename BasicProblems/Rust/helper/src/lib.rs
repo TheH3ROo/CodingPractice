89a3f18d32ca13f2dad40e0c4e708fd8dac7a7b4
rab.peti <43448373+TheH3ROo@users.noreply.github.com>
2023-09-26 20:02:52 +0200

    helper project added, common fn moved to helper project and used from there

---
+++ BasicProblems/Rust/helper/src/lib.rs
@@ -0,0 +1,15 @@
+use std::io;
+
+pub fn get_input_from_user() -> String {
+    // Create a mutable String to store the user's input
+    let mut input = String::new();
+
+    // Prompt the user for input
+    println!("Please enter a string:");
+
+    // Read a line from the standard input and store it in the 'input' variable
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    // Print the user's input
+    return input;
+}
--- BasicProblems/Rust/palindrome/src/main.rs
+++ BasicProblems/Rust/palindrome/src/main.rs
@@ -1,7 +1,7 @@
-use std::io;
+extern crate helper;
 
 fn main() {
-    let input: String = get_input_from_user();
+    let input: String = helper::get_input_from_user();
     print!("{}", input);
     let palindrome: bool = is_palindrome(&input);
     println!("{}", palindrome);
@@ -14,20 +14,6 @@ fn main() {
     }
 }
 
-fn get_input_from_user() -> String {
-    // Create a mutable String to store the user's input
-    let mut input = String::new();
-
-    // Prompt the user for input
-    println!("Please enter a string:");
-
-    // Read a line from the standard input and store it in the 'input' variable
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-
-    // Print the user's input
-    return input;
-}
-
 fn is_palindrome(input: &str) -> bool {
     let trimmed_input = input.trim();
     let reversed = reverse(&trimmed_input);
@@ -38,14 +24,15 @@ fn reverse(s: &str) -> String {
     s.chars().rev().collect()
 }
 
-fn is_palindrome_to_middle(input: String) -> bool {
-    for i in 0..input.len()/2 {
-        let ch = input.chars().nth(i);
-        let ch2 = input.chars().nth(input.len() - i);
+//TODO: Finish this fn
+// fn is_palindrome_to_middle(input: String) -> bool {
+//     for i in 0..input.len()/2 {
+//         let ch = input.chars().nth(i);
+//         let ch2 = input.chars().nth(input.len() - i);
         
-        println!("ch:{}", ch.unwrap());
-        println!("ch2:{}", ch2.unwrap());
-        if ch != ch2 { return false; }
-    }
-    return true;
-}
+//         println!("ch:{}", ch.unwrap());
+//         println!("ch2:{}", ch2.unwrap());
+//         if ch != ch2 { return false; }
+//     }
+//     return true;
+// }
