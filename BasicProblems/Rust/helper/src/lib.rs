4cea4965e457f6843f352646a05e194d7d64d7ce
rab.peti <43448373+TheH3ROo@users.noreply.github.com>
2023-09-26 21:08:32 +0200

    Add fn to get number input from user and specify original fn for string

--- BasicProblems/Rust/helper/src/lib.rs
+++ BasicProblems/Rust/helper/src/lib.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-pub fn get_input_from_user() -> String {
+pub fn get_string_from_user() -> String {
     // Create a mutable String to store the user's input
     let mut input = String::new();
 
@@ -12,6 +12,20 @@ pub fn get_input_from_user() -> String {
 
     // Print the user's input
     return input;
+}
+
+pub fn get_number_from_user() -> i32 {
+    // Create a mutable String to store the user's input
+    let mut input = String::new();
+
+    // Prompt the user for input
+    println!("Please enter a number:");
+
+    // Read a line from the standard input and store it in the 'input' variable
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    // Print the user's input
+    return input.trim().parse().expect("Invalid input");
 }
 
 pub trait ReverseStringExt {
