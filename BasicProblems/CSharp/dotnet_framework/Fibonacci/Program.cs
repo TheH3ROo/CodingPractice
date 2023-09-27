2ac6c787970e082f5a82a0aff69542389a979257
rab.peti <43448373+TheH3ROo@users.noreply.github.com>
2023-09-27 19:03:57 +0200

    Add Fibonacci C# project

---
+++ BasicProblems/CSharp/dotnet_framework/Fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Helpers;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NthFibonacci(Helper.GetNumberFromUser()));
+            Console.Read();
+        }
+
+        static int NthFibonacci(int index)
+        {
+            if (index < 0) return 0;
+            if (index == 1) return 1;
+            return NthFibonacci(index - 1) + (NthFibonacci(index - 2));
+        }
+    }
+}
